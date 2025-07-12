3350686bac2d83d2dffd60dc4cc927d613c2843b
Marc Dwain <marcdwainm@gmail.com>
2025-07-12 20:20:40 +0800

    Added Cursor and Minimal Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+// REACT
+import { useState } from "react";
+
+// SECTIONS
 import Intro from "./sections/Intro";
+import Landing from "./sections/Landing";
+import IntroTransition from "./transitions/IntroTransition";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function App() {
 
-  return <div className="min-h-screen bg-gray-200 text-gray-800">
-    <Intro />
-  </div>
+    const [transition, setTransition] = useState()
+    const [currentPage, setCurrentPage] = useState("intro");
+
+    
+    const handlePageChange = (page) => {
+        
+        // Run animation first
+        setTransition()
+        
+        // In the middle of the animation, change page accordinly.
+        setTimeout(() => {
+            setCurrentPage(page)
+        }, 1500)
+    }
+    
+    const pageMapping = {
+        intro: () => <Intro handlePageChange={(page) => handlePageChange(page)} />,
+        landing: () => <Landing />
+    }
+
+    useGSAP(() => {
+
+        gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+
+        let xTo = gsap.quickTo(".cursor", "x", {duration: 0.6, ease: "power3"}),
+            yTo = gsap.quickTo(".cursor", "y", {duration: 0.6, ease: "power3"});
+
+        window.addEventListener("mousemove", e => {
+            xTo(e.clientX);
+            yTo(e.clientY);
+        });
+    })
+    
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-white to-[#cac9c5] text-black">
+            {pageMapping[currentPage]?.()}
+
+            {transition}
+
+            <div className="cursor w-[100px] h-[100px] fixed top-0 left-0 pointer-events-none z-[1000] rounded-full bg-red-900 mix-blend-difference" />
+        </div>
+    );
 }
--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/all";
+import { SplitText } from "gsap/all"
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 
-export default function Intro() {
+export default function Intro({ handlePageChange }) {
 
     const [isClickable, setIsClickable] = useState(false);
     const [hasHovered, setHasHovered] = useState(false);
@@ -32,10 +33,10 @@ export default function Intro() {
             opacity: 0,
             stagger: { each: 0.02, from: "center" },
             ease: "power4.inOut",
-            duration: 1.5,
+            duration: 1.3,
             delay: 0.9,
             onComplete: () => setIsClickable(true),
-        }, "<")
+        }, "<")  
     }, []);
 
     useEffect(() => {
@@ -61,15 +62,21 @@ export default function Intro() {
             duration: 0.5,
             ease: "power4.inOut",
         })
+
+        gsap.to("#difference", {
+            clipPath: hasHovered ? "polygon(0% 40%, 100% 40%, 100% 60%, 0% 60%)" : "polygon(0% 50%, 100% 50%, 100% 50%, 0% 50%)",
+            duration: 0.5,
+            ease: "power4.inOut",
+        })
     }, [hasHovered, isClickable]);
 
     return (
-        <div className="intro-wrapper">
-            <div className="bg-gray-200 h-full w-full flex-center overflow-hidden">
+        <div className="page-wrapper">
+            <div className="bg-transparent h-full w-full flex-center overflow-hidden">
                 <svg className="w-16 h-16 rotate-180 translate-x-[2000px]" id="logo-bracket-1" fill="#222123" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="m4.8 21.57 2.422 2.43 11.978-12-11.978-12-2.422 2.43 9.547 9.57z" />
                 </svg>
-                <div className="relative min-w-[270px] cursor-pointer" onMouseEnter={() => setHasHovered(true)} onMouseLeave={() => setHasHovered(false)}>
+                <div className="relative min-w-[270px] cursor-pointer" onMouseEnter={() => setHasHovered(true)} onMouseLeave={() => setHasHovered(false)} onClick={() => handlePageChange("landing")}>
                     <h1 id="dwain-dev" ref={dwainRef} className="absolute overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 color-black font-logo text-center whitespace-nowrap w-max md:text-[4.5rem] text-6xl tracking-tighter">
                         DWAIN.DEV
                     </h1>
@@ -82,6 +89,16 @@ export default function Intro() {
                 <svg className="w-16 h-16 -translate-x-[2000px]" id="logo-bracket-2" fill="#222123" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="m4.8 21.57 2.422 2.43 11.978-12-11.978-12-2.422 2.43 9.547 9.57z" />
                 </svg>
+                <div id="difference" style={{ clipPath: "polygon(0% 50%, 100% 50%, 100% 50%, 0% 50%)" }} className="absolute w-full h-dvh bg-[#f3f3f3] mix-blend-difference cursor-pointer" onMouseEnter={() => setHasHovered(true)} onMouseLeave={() => setHasHovered(false)} onClick={() => handlePageChange("landing")}>
+                    <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-logo md:text-[15rem] text-[18rem] text-center font-extrabold leading-none mix-blend-color-dodge">
+                        KNOW MORE
+                    </h1>
+                </div>
+                
+                <p className="absolute top-10 left-10 font-secondary leading-[1] md:text-[14px] text-[12px]">
+                    <ArrowLongRightIcon className="w-5 h-5"/>
+                    Portfolio <br/> v-1.0.0
+                </p>
             </div>
         </div>
     );

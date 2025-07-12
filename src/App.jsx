7c626273d6169b8779b2da55db3a683ac691b37d
Marc Dwain <marcdwainm@gmail.com>
2025-07-12 20:25:36 +0800

    Segregated Cursor Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import { useState } from "react";
 import Intro from "./sections/Intro";
 import Landing from "./sections/Landing";
 import IntroTransition from "./transitions/IntroTransition";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import Cursor from "./components/Cursor";
 
 export default function App() {
 
@@ -30,26 +29,13 @@ export default function App() {
         landing: () => <Landing />
     }
 
-    useGSAP(() => {
-
-        gsap.set(".cursor", {xPercent: -50, yPercent: -50});
-
-        let xTo = gsap.quickTo(".cursor", "x", {duration: 0.6, ease: "power3"}),
-            yTo = gsap.quickTo(".cursor", "y", {duration: 0.6, ease: "power3"});
-
-        window.addEventListener("mousemove", e => {
-            xTo(e.clientX);
-            yTo(e.clientY);
-        });
-    })
-    
     return (
         <div className="min-h-screen bg-gradient-to-b from-white to-[#cac9c5] text-black">
             {pageMapping[currentPage]?.()}
 
             {transition}
 
-            <div className="cursor w-[100px] h-[100px] fixed top-0 left-0 pointer-events-none z-[1000] rounded-full bg-red-900 mix-blend-difference" />
+            <Cursor />
         </div>
     );
 }
